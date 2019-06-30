150b1302044d68e52a8a0c5231d99bf48e8efa65
TongDaxia <tong_y_g@163.com>
2019-07-01 00:07:22 +0800

    第一个脚本文件

---
+++ ElasticStart.bat
@@ -0,0 +1,3 @@
+start cmd /k D:\dev\elasticsearch-7.2.0\bin\elasticsearch.bat 
+start cmd /k "cd /d D:\dev\elasticsearch-head-master&&grunt server" 
+start cmd /k "cd /d D:\dev\kibana-7.2.0\bin&&kibana.bat"
